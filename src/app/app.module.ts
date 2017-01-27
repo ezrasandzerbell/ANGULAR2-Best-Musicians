3976e45be349e1412566dafe8058d7c8f25bf726
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-01-27 08:03:32 -0800

    add components to template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent,
+    AboutComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
