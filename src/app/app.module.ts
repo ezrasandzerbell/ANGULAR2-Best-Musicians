cdedf383211c23ca409c9ce7237d726d07fbe34e
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-01-27 08:13:09 -0800

    complete basic template with components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
 import { AboutComponent } from './about/about.component';
@@ -20,7 +20,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
